f25c44eda09074e0e7232705424ff9a6945e532d
Ashe Connor <ashe@kivikakk.ee>
2024-01-08 11:43:02 +1100

    flake.nix: working from nd, not nb.

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Has Lily had beffast yet?";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    arduino-nix.url = github:clerie/arduino-nix?ref=clerie/arduino-env;
+    arduino-library-index = {
+      url = github:bouk/arduino-indexes/library_index;
+      flake = false;
+    };
+    arduino-package-index = {
+      url = github:bouk/arduino-indexes/package_index;
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    arduino-nix,
+    arduino-package-index,
+    arduino-library-index,
+    ...
+  }: let
+    overlays = [
+      arduino-nix.overlay
+      (arduino-nix.mkArduinoPackageOverlay "${arduino-package-index}/package_index.json")
+      (arduino-nix.mkArduinoPackageOverlay (builtins.fetchTree {
+        type = "file";
+        url = "http://raw.githubusercontent.com/SolderedElectronics/Dasduino-Board-Definitions-for-Arduino-IDE/master/package_Dasduino_Boards_index.json";
+        narHash = "sha256-qMvkUTEMDOFtNBxFwF7bVmJGRekZW7r/GErSEe5PD7o=";
+      }))
+      (arduino-nix.mkArduinoLibraryOverlay "${arduino-library-index}/library_index.json")
+    ];
+  in
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system overlays;};
+
+        arduinoEnv = pkgs.mkArduinoEnv {
+          libraries = with pkgs.arduinoLibraries; [
+            (arduino-nix.latestVersion InkplateLibrary)
+          ];
+
+          packages = with pkgs.arduinoPackages; [
+            (arduino-nix.latestVersion platforms.Inkplate_Boards.esp32)
+          ];
+        };
+      in {
+        formatter = pkgs.alejandra;
+
+        packages.default = arduinoEnv.buildArduinoSketch {
+          name = "beffast";
+          src = ./. + "/beffast";
+          fqbn = "Inkplate_Boards:esp32:Inkplate6";
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [arduinoEnv];
+        };
+      }
+    );
+}
